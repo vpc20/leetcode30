56359a9934f91bdd9a4bbbde8cfe79b8a2aa6daa
vpc <cuevasvp@gmail.com>
2020-04-11 18:24:40 +0800

    alternate solution

--- MinStack.py
+++ MinStack.py
@@ -17,35 +17,57 @@
 
 class MinStack:
     def __init__(self):
-        self.arr = []
-        self.min = 0
+        self.stack = []
 
     def push(self, x: int) -> None:
-        self.arr.append(x)
-        if len(self.arr) == 1:
-            self.min = self.arr[0]
+        if self.stack:
+            self.stack.append((x, min(x, self.getMin())))
         else:
-            self.min = min(self.min, x)
+            self.stack.append((x, x))
 
     def pop(self) -> None:
-        if len(self.arr) > 0:
-            popval = self.arr.pop()
-            if len(self.arr) == 0:
-                self.min = None
-            else:
-                self.min = min(self.arr)
+        if self.stack:
+            self.stack.pop()
 
     def top(self) -> int:
-        return self.arr[-1]
+        return self.stack[-1][0]
 
     def getMin(self) -> int:
-        return self.min
+        return self.stack[-1][1]
+
+
+# class MinStack:
+#     def __init__(self):
+#         self.arr = []
+#         self.min = 0
+#
+#     def push(self, x: int) -> None:
+#         self.arr.append(x)
+#         if len(self.arr) == 1:
+#             self.min = self.arr[0]
+#         else:
+#             self.min = min(self.min, x)
+#
+#     def pop(self) -> None:
+#         if len(self.arr) > 0:
+#             popval = self.arr.pop()
+#             if len(self.arr) == 0:
+#                 self.min = None
+#             else:
+#                 self.min = min(self.arr)
+#
+#     def top(self) -> int:
+#         return self.arr[-1]
+#
+#     def getMin(self) -> int:
+#         return self.min
 
 
 minStack = MinStack()
 minStack.push(-2)
 minStack.push(0)
 minStack.push(-3)
+print(minStack.stack)
 print(minStack.getMin())  # returns -3
 minStack.pop()
 print(minStack.top())  # returns 0
