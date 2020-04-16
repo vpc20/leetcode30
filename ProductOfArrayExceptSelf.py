189f6e567d36de829a189366a67d0e5f58a0b96f
vpc <cuevasvp@gmail.com>
2020-04-16 19:22:25 +0800

    alternate solutions

--- ProductOfArrayExceptSelf.py
+++ ProductOfArrayExceptSelf.py
@@ -27,35 +27,97 @@ def productExceptSelf_naive(nums):
     return output
 
 
+# def productExceptSelf(nums):
+#     output = [0] * len(nums)
+#     if nums.count(0) > 1:
+#         return output
+#     if nums.count(0) == 1:
+#         prod = 1
+#         for i in range(len(nums)):
+#             if nums[i] == 0:
+#                 zero_idx = i
+#             else:
+#                 prod *= nums[i]
+#         output[zero_idx] = prod
+#         return output
+#
+#     pref = [nums[0]]  # prefix product
+#     for i in range(1, len(nums)):
+#         pref.append(nums[i] * pref[-1])
+#     # pref = nums.copy()  # prefix product
+#     # for i in range(1, len(pref)):
+#     #     pref[i] *= pref[i - 1]
+#     return [int(pref[-1] / nums[i]) for i in range(len(nums))]
+
+
 def productExceptSelf(nums):
-    output = [0] * len(nums)
-    if nums.count(0) > 1:
-        return output
-    if nums.count(0) == 1:
-        prod = 1
-        for i in range(len(nums)):
-            if nums[i] == 0:
-                zero_idx = i
-            else:
-                prod *= nums[i]
-        output[zero_idx] = prod
-        return output
+    pref = nums.copy()  # prefix product
+    for i in range(1, len(pref)):
+        pref[i] *= pref[i - 1]
 
-    pref = [nums[0]]  # prefix product
-    for i in range(1, len(nums)):
-        pref.append(nums[i] * pref[-1])
-    # pref = nums.copy()  # prefix product
-    # for i in range(1, len(pref)):
-    #     pref[i] *= pref[i - 1]
-    return [int(pref[-1] / nums[i]) for i in range(len(nums))]
+    suff = nums.copy()  # suffix product
+    for i in range(-2, -len(suff) - 1, -1):
+        suff[i] *= suff[i + 1]
+
+    result = []
+    for i in range(len(nums)):
+        if i == 0:
+            result.append(suff[i + 1])
+        elif i == len(nums) - 1:
+            result.append(pref[i - 1])
+        else:
+            result.append(pref[i - 1] * suff[i + 1])
+    return result
 
 
-nums = [1, 2, 3, 4]
+# def productExceptSelf(nums):
+#     pref = nums.copy()  # prefix product
+#     suff = nums.copy()  # suffix product
+#     for i in range(1, len(pref)):
+#         pref[i] *= pref[i - 1]
+#         suff[-i - 1] *= suff[-i]
+#
+#     result = []
+#     for i in range(len(nums)):
+#         if i == 0:
+#             result.append(suff[i + 1])
+#         elif i == len(nums) - 1:
+#             result.append(pref[i - 1])
+#         else:
+#             result.append(pref[i - 1] * suff[i + 1])
+#     return result
+
+
+# def productExceptSelf(nums):
+#     pref = [1] + [0] * (len(nums) - 1)  # prefix product
+#     suff = [0] * (len(nums) - 1) + [1]  # suffix product
+#
+#     for i in range(1, len(nums)):
+#         pref[i] = nums[i - 1] * pref[i - 1]
+#         suff[-i - 1] = nums[-i] * suff[-i]
+#
+#     return [pref[i] * suff[i] for i in range(len(nums))]
+
+
+# nums = [1, 2, 3, 4]
 # nums = [1, 0, 2, 3]
 # nums = [1, 0, 2, 0]
 # nums = [1, 0]
 # nums = [1, 1]
 # nums = [9, 0, -2]
+nums = [1, 2, 3, 4, 5]
 print('input', nums)
 print('correct', productExceptSelf_naive(nums))
 print(productExceptSelf(nums))
+
+#   1     2     3     4     5
+
+#  120   60    40    30    24
+
+#   1     2     6    24   120   --- pref
+
+#  120   120   60    20     5   --- suff
+
+
+#  0    1    2    3    4
+# -5   -4   -3   -2   -1
