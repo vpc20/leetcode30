ed122abe35ade2aa3e29e28fbe6c01b2cb589aba
vpc <cuevasvp@gmail.com>
2020-04-27 07:10:05 +0800

    test script with random data

---
+++ test_JumpGame.py
@@ -0,0 +1,15 @@
+from unittest import TestCase
+
+from JumpGame import can_jump, can_jump_dyna, can_jump_dyna1
+from RandomData import random_int_array
+
+
+class Test(TestCase):
+    def test_can_jump(self):
+        for _ in range(10000):
+            arr = random_int_array(10, 10)
+            if arr:
+                # print(arr)
+                # print(can_jump(arr))
+                # self.assertEqual(can_jump(arr), can_jump_dyna(arr))
+                self.assertEqual(can_jump(arr), can_jump_dyna1(arr))
