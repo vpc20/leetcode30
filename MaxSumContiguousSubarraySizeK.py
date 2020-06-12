941fa96d2f14a65231c38f209ea8cd1fef78231b
vpc <cuevasvp@gmail.com>
2020-06-12 11:53:36 +0800

    random data test

--- MaxSumContiguousSubarraySizeK.py
+++ MaxSumContiguousSubarraySizeK.py
@@ -1,7 +1,24 @@
 import sys
 
+from RandomData import random_int_array
+
+
+def max_sum_naive(arr, k):
+    if k > len(arr):
+        return None
+    maxsum = -sys.maxsize
+    for i in range(len(arr) - k + 1):
+        maxsum = max(maxsum, sum(arr[i:i + k]))
+    return maxsum
+
+
+# n = 10   k = 3
+# 0 1 2 3 4 5 6 7 8 9
+
 
 def max_sum(arr, k):
+    if k > len(arr):
+        return None
     curr_sum = sum(arr[:k])  # sliding window of length k
     max_sum = curr_sum
     l = 1
@@ -15,4 +32,20 @@ def max_sum(arr, k):
     return max_sum
 
 
-print(max_sum([4, 2, 1, 7, 8, 1, 2, 8, 1, 0], 3))
+assert max_sum_naive([1, 2, 3], 3) == 6
+assert max_sum([1, 2, 3], 3) == 6
+
+assert max_sum_naive([1, 2, 3, 4], 3) == 9
+assert max_sum([1, 2, 3, 4], 3) == 9
+
+assert max_sum_naive([4, 3, 2, 1], 3) == 9
+assert max_sum([4, 3, 2, 1], 3) == 9
+
+assert max_sum_naive([4, 2, 1, 7, 8, 1, 2, 8, 1, 0], 3) == 16
+assert max_sum([4, 2, 1, 7, 8, 1, 2, 8, 1, 0], 3) == 16
+
+for _ in range(10000):
+    arr = random_int_array(20, 15)
+    print(arr)
+    for k in range(1, 6):
+        assert max_sum_naive(arr, k) == max_sum(arr, k)
